016f7caa7ebcafe0811dbde05b16dbde30b6d30e
luanbrde <127901732+luanbrde@users.noreply.github.com>
2024-03-21 22:27:31 -0300

    Add files via upload

---
+++ src/main/java/com/mouseLuan/curso/config/TestConfig.java
@@ -0,0 +1,40 @@
+package com.mouseLuan.curso.config;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.mouseLuan.curso.entities.User;
+import com.mouseLuan.curso.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository UserRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(1L, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(2L, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		
+		
+		UserRepository.saveAll(Arrays.asList(u1,u2));
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
---
+++ src/main/java/com/mouseLuan/curso/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package com.mouseLuan.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mouseLuan.curso.entities.User;
+
+public interface  UserRepository extends JpaRepository<User, Long> {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
